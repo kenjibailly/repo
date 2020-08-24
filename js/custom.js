c59e79f89488ddbdf90e1e428b7e436424ff162c
Kenji Bailly <kenjibailly@gmail.com>
2020-08-24 05:11:18 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -30,6 +30,26 @@ jQuery(function ($) { "use strict";
 	// 	cursorborder: 0,
 	// });
 
+	/* ========================================================================= */
+	/*	Preloader Animation
+	/* ========================================================================= */
+
+	$(window).on('load', function () {
+		if ($('.cover').length) {
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function () {
+			setTimeout(function () {
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
 
 	/* ========================================================================= */
 	/*	Scroll Up / Back to top
